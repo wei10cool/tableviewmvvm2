88407ee94c43122d97184d56e676d34bf8732cb2
wei10cool <x215033@yahoo.com.tw>
2018-03-06 00:36:47 +0800

    first commit

---
+++ tableviewmvvm/TodoItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoItemTableViewCell.swift
+//  tableviewmvvm
+//
+//  Created by jianwei on 05/03/2018.
+//  Copyright © 2018 jianwei. All rights reserved.
+//
+
+import UIKit
+
+class TodoItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ tableviewmvvm/TodoVIewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TodoVIewModel.swift
+//  tableviewmvvm
+//
+//  Created by jianwei on 05/03/2018.
+//  Copyright © 2018 jianwei. All rights reserved.
+//
+
+import Foundation
